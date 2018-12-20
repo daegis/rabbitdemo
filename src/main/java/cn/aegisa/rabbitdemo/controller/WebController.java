fee567b124c75442e2758296ff53a0e0b25af249
xianyingda <xianyingda@shouqiev.com>
2018-12-20 14:06:18 +0800

    增加版本标识

---
+++ src/main/java/cn/aegisa/rabbitdemo/controller/WebController.java
@@ -0,0 +1,28 @@
+package cn.aegisa.rabbitdemo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Using IntelliJ IDEA.
+ *
+ * @author XIANYINGDA at 2018/12/20 14:04
+ */
+@Controller
+@Slf4j
+public class WebController {
+
+    @RequestMapping("/build")
+    public Object testBuild() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("time", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        map.put("version", "111111");
+        return map;
+    }
+}
