825bf15fd62369e0a4da32eea4a50b1eb4c37b1e
xianyingda <xianyingda@shouqiev.com>
2018-12-20 15:11:10 +0800

    bug fix

--- src/main/java/cn/aegisa/rabbitdemo/controller/WebController.java
+++ src/main/java/cn/aegisa/rabbitdemo/controller/WebController.java
@@ -3,6 +3,7 @@ package cn.aegisa.rabbitdemo.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -19,6 +20,7 @@ import java.util.Map;
 public class WebController {
 
     @RequestMapping("/build")
+    @ResponseBody
     public Object testBuild() {
         Map<String, Object> map = new HashMap<>();
         map.put("time", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
